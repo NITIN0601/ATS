1dca8e41cc2a64b055c4e03fbf642ce72d6da62f
Nitin <dmnitin97@gmail.com>
2024-01-17 17:35:08 -0500

    Version2_1

--- app.py
+++ app.py
@@ -8,14 +8,15 @@ import io
 from PIL import Image
 import pdf2image
 import google.generativeai as genai
+import prompt_techinque as pt
 
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-def get_gemini_response(input, pdf_content, prompt):
+def get_model_response(input, pdf_content, prompt):
     """
-    Input
-    Output
+    Input: PDF
+    Output: Text response
     """
     model = genai.GenerativeModel('gemini-pro-vision')
     response = model.generate_content([input, pdf_content[0],prompt])
@@ -39,11 +40,20 @@ def input_pdf_setup(upload_file):
                 "mime_type": "image/jpeg",
                 "data": base64.b64encode(img_byte_arr).decode() #encoding to base64
             }
-
         ]
         return pdf_parts
     else:
         raise FileNotFoundError("No file uploaded")
+
+
+def logic_code(prompt_value):
+    if uploaded_file is not None:
+        pdf_content = input_pdf_setup(uploaded_file)
+        response = get_model_response(prompt_value,pdf_content,input_text)
+        st.subheader("The Response is : ")
+        st.write(response)
+    else:
+        st.write("Please Upload the Resume file")
 
 
 
@@ -52,43 +62,18 @@ def input_pdf_setup(upload_file):
 
 st.set_page_config(page_title="ATS Resume")
 st.header("ATS Tracking System")
-input_text = st.text_area("Job Description: ",key="input")
+input_job_text = st.text_input("Enter the Job Role",key="input_job_role") 
+input_text = st.text_area("Job Description: ",key="input_job_desc")
 uploaded_file = st.file_uploader("Upload your resume into PDF ", type=["pdf"])
 
 if uploaded_file is not None:
     st.write("PDF Uploaded Sucessfully")
 
 submit_1 = st.button("Tell Me About the Resume")
-#submit_2 = st.button("How Can I improvise my skills")
-submit_3 = st.button("Percentage Match")
-
-input_prompt_1 = """
-                You are an experienced HR with Tech Experience in the field of any one job role of Data Scientist or Data Analyst or Data Engineer,
-                Your taks is to review the provided resume against the job description for these profiles.
-                Please share your professional evalluation on whether the candidate's profile aligns with the role.
-                Highlight the strengths and weakness of the applicant in relation to the specified job requirements.
-                """
-
-input_prompt_3 = """
-                You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of any one job role Data Science, Data Analyst, Data Engineer and ATS functionality, 
-                Your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
-                the job description. First the output should come as percentage and then list me out keywords missing and last final thoughts.
-                """
+submit_2 = st.button("Percentage Match")
 
 if submit_1:
-    if uploaded_file is not None:
-        pdf_content = input_pdf_setup(uploaded_file)
-        response = get_gemini_response(input_prompt_1,pdf_content,input_text)
-        st.subheader("The Response is : ")
-        st.write(response)
-    else:
-        st.write("Please Upload the Resume file")
-
-elif submit_3:
-    if uploaded_file is not None:
-        pdf_content = input_pdf_setup(uploaded_file)
-        response = get_gemini_response(input_prompt_3,pdf_content,input_text)
-        st.subheader("The Response is : ")
-        st.write(response)
-    else:
-        st.write("Please Upload the Resume file")
+    logic_code(pt.prompt1(input_job_text))
+elif submit_2:
+    logic_code(pt.prompt2(input_job_text))
+    
---
+++ prompt_techinque.py
@@ -0,0 +1,45 @@
+def prompt1(input_job_role):
+    input_prompt_1 = """
+                    Outline resume screening for resume with the Job Description :
+                    List out all the issues in resume with respective to Job Description.
+
+                    1. Identify technical requirements
+                    2. Check experience fit
+                    3. Surface experience gaps    
+                    4. Assess projects if fresher
+                    5. Recommend improvements  
+
+                    If resume does not fit job description, state key gaps.
+                    Otherwise provide focused recommendations to better align. 
+                    Structure output into : Requirements, list the Gaps, list the Changes Required specifing the tech keywords, Recommendations.
+                    """
+    return input_prompt_1
+
+
+def prompt2(input_job_role):
+    input_prompt_1 = """
+                    You are an ATS resume scanner with expertise in technical requirement analysis. 
+                    Compare the resume to the job description judging match based on:
+
+                    1. Technical Skills (70% weight)
+                    - Identify missing technical keywords. 
+                    - Reduce match score if critical skills are not present.
+
+                    2. Experience Level (20% weight)  
+                    - Check alignment with years required as per job description.
+                    - For every year of experience underrequired, reduce match score.
+                    - If meets/exceeds, do not adjust score positively.
+
+                    3. Education Level (10% weight)
+                    - Validate if educational criteria are met based on job needs.
+                    - For every educational degree missing, marginally reduce match score.
+                    - If education level matches/surpasses, do not increase score.
+
+                    First, state the percent match score after judging missing criteria weighted above.
+                    Next, list all unmet technical skills and experience gaps. 
+                    Finally, provide closing recommendations on areas of mismatch for improving resume relevance.  
+
+                    Ignore any textual tables, charts or visual formatting when scanning. Call those out separately after final thoughts.
+                    """     
+    return input_prompt_1
+
